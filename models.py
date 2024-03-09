cd2c044313448e3d027a9067ff7917cbb9063072
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-09 18:08:46 +0100

    Update

--- models.py
+++ models.py
@@ -1,55 +1,64 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-
-SQLALCHEMY_DATABASE_URL = 'sqlite:///university_database.db'
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey, Table
 
 Base = declarative_base()
 
-# Connect to the database
-Session = sessionmaker(bind=engine)
-session = Session()
+'''
+Association table for the many-to-many relationship
+between teachers and disciplines
+'''
+teacher_discipline = Table(
+    'teacher_discipline',
+    Base.metadata,
+    Column('teacher_id', Integer, ForeignKey('teachers.id'), primary_key=True),
+    Column('discipline_id', Integer, ForeignKey('disciplines.id'),
+           primary_key=True)
+)
 
 
 class Group(Base):
     __tablename__ = 'groups'
     id = Column(Integer, primary_key=True)
-    group_name = Column(String, unique=True)
-    students = relationship("Student", backref="group")
+    name = Column(String(50), unique=True)
+    students = relationship('Student', backref='group')
 
     def __str__(self):
-        return self.group_name
+        return self.name
 
 
 class Student(Base):
     __tablename__ = 'students'
     id = Column(Integer, primary_key=True)
-    full_name = Column(String)
+    name = Column(String(150))
     group_id = Column(Integer, ForeignKey('groups.id'))
+    grades = relationship('Grade', back_populates='student')
 
     def __str__(self):
-        return self.full_name
+        return self.name
 
 
 class Teacher(Base):
     __tablename__ = 'teachers'
     id = Column(Integer, primary_key=True)
-    full_name = Column(String)
-    disciplines = relationship("Discipline", backref="teacher")
+    name = Column(String(150))
+    disciplines = relationship('Discipline',
+                               secondary=teacher_discipline,
+                               back_populates='teachers')
 
     def __str__(self):
-        return self.full_name
+        return self.name
 
 
 class Discipline(Base):
     __tablename__ = 'disciplines'
     id = Column(Integer, primary_key=True)
-    discipline_name = Column(String, unique=True)
-    teacher_id = Column(Integer, ForeignKey('teachers.id'))
+    name = Column(String(50), unique=True)
+    teachers = relationship('Teacher',
+                            secondary=teacher_discipline,
+                            back_populates='disciplines')
+    grades = relationship('Grade', back_populates='discipline')
 
     def __str__(self):
         return self.discipline_name
@@ -62,9 +71,5 @@ class Grade(Base):
     discipline_id = Column(Integer, ForeignKey('disciplines.id'))
     grade = Column(Integer)
     grade_date = Column(Date)
-    student = relationship("Student", backref="grades")
-    discipline = relationship("Discipline", backref="grades")
-
-
-# Create tables
-Base.metadata.create_all(engine)
+    student = relationship("Student", back_populates="grades")
+    discipline = relationship("Discipline", back_populates="grades")
