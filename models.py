6497c684b0828ccbb755db5080784d5d5d21c09e
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-09 21:18:59 +0100

    Update models

--- models.py
+++ models.py
@@ -1,28 +1,16 @@
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import ForeignKey, Table
+from sqlalchemy.sql.schema import ForeignKey
 
 Base = declarative_base()
-
-'''
-Association table for the many-to-many relationship
-between teachers and disciplines
-'''
-teacher_discipline = Table(
-    'teacher_discipline',
-    Base.metadata,
-    Column('teacher_id', Integer, ForeignKey('teachers.id'), primary_key=True),
-    Column('discipline_id', Integer, ForeignKey('disciplines.id'),
-           primary_key=True)
-)
 
 
 class Group(Base):
     __tablename__ = 'groups'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    students = relationship('Student', backref='group')
+    name = Column(String(50), nullable=False)
+    students = relationship('Student', back_populates='group')
 
     def __str__(self):
         return self.name
@@ -31,8 +19,9 @@ class Group(Base):
 class Student(Base):
     __tablename__ = 'students'
     id = Column(Integer, primary_key=True)
-    name = Column(String(150))
-    group_id = Column(Integer, ForeignKey('groups.id'))
+    name = Column(String(150), nullable=False)
+    group_id = Column(Integer, ForeignKey('groups.id', ondelete="CASCADE"))
+    group = relationship('Group', back_populates='students')
     grades = relationship('Grade', back_populates='student')
 
     def __str__(self):
@@ -42,10 +31,8 @@ class Student(Base):
 class Teacher(Base):
     __tablename__ = 'teachers'
     id = Column(Integer, primary_key=True)
-    name = Column(String(150))
-    disciplines = relationship('Discipline',
-                               secondary=teacher_discipline,
-                               back_populates='teachers')
+    name = Column(String(150), nullable=False)
+    disciplines = relationship('Discipline', back_populates='teacher')
 
     def __str__(self):
         return self.name
@@ -54,22 +41,25 @@ class Teacher(Base):
 class Discipline(Base):
     __tablename__ = 'disciplines'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    teachers = relationship('Teacher',
-                            secondary=teacher_discipline,
-                            back_populates='disciplines')
+    name = Column(String(50), nullable=False, unique=True)
+    teacher_id = Column(Integer, ForeignKey('teachers.id', ondelete="CASCADE"))
+    teacher = relationship('Teacher', back_populates='disciplines')
     grades = relationship('Grade', back_populates='discipline')
 
     def __str__(self):
-        return self.discipline_name
+        return self.name
 
 
 class Grade(Base):
     __tablename__ = 'grades'
     id = Column(Integer, primary_key=True)
-    student_id = Column(Integer, ForeignKey('students.id'))
-    discipline_id = Column(Integer, ForeignKey('disciplines.id'))
-    grade = Column(Integer)
-    grade_date = Column(Date)
+    grade = Column(Integer, nullable=False)
+    grade_date = Column(Date, nullable=False)
+    student_id = Column(Integer, ForeignKey('students.id', ondelete="CASCADE"))
+    discipline_id = Column(
+        Integer, ForeignKey('disciplines.id', ondelete="CASCADE"))
     student = relationship("Student", back_populates="grades")
     discipline = relationship("Discipline", back_populates="grades")
+
+    def __str__(self):
+        return f"{self.grade}, ({self.grade_date.strftime('%Y-%m-%d')})"
